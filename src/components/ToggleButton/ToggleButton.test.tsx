aa2ce2410b5356fbd05859982aaa3812e1a975fa
Marten <derapfelesser@aol.de>
2023-09-19 17:07:07 +0200

    test(toggle-button): onChange function gets called

--- src/components/ToggleButton/ToggleButton.test.tsx
+++ src/components/ToggleButton/ToggleButton.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import ToggleButton from './ToggleButton';
 
 test('represents active state passed as props', () => {
@@ -11,6 +12,13 @@ test('represents active state passed as props', () => {
   expect(toggleButtonDisabled).toHaveAttribute('aria-checked', 'false');
 });
 
-test('calls onChange when element is clicked', () => {
-  expect(true).toEqual(true);
+test('calls onChange when element is clicked', async () => {
+  const mockFn = jest.fn();
+  render(<ToggleButton activated={true} onChange={mockFn} />);
+  const user = userEvent.setup();
+
+  const toggleButtonDisabled = screen.getByRole('checkbox');
+  await user.click(toggleButtonDisabled);
+
+  expect(mockFn).toHaveBeenCalled();
 });
