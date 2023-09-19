4d481807594f76c1f8e5e85a58c11a8f04ea2a5e
Marten <derapfelesser@aol.de>
2023-09-19 16:49:59 +0200

    test(toggle-button): active state prop has effect

--- src/components/ToggleButton/ToggleButton.test.tsx
+++ src/components/ToggleButton/ToggleButton.test.tsx
@@ -1,1 +1,16 @@
 import { render, screen } from '@testing-library/react';
+import ToggleButton from './ToggleButton';
+
+test('represents active state passed as props', () => {
+  const { rerender } = render(<ToggleButton activated={true} onChange={() => {}} />);
+  const toggleButtonActive = screen.getByRole('checkbox');
+  expect(toggleButtonActive).toHaveAttribute('aria-checked', 'true');
+
+  rerender(<ToggleButton activated={false} onChange={() => {}} />);
+  const toggleButtonDisabled = screen.getByRole('checkbox');
+  expect(toggleButtonDisabled).toHaveAttribute('aria-checked', 'false');
+});
+
+test('calls onChange when element is clicked', () => {
+  expect(true).toEqual(true);
+});
