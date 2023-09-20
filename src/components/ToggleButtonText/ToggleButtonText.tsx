7dfe079fbc925382e93a57c18cb06bb5db19bf80
Marten <derapfelesser@aol.de>
2023-09-20 11:21:42 +0200

    feat(toggle-button-text): create toggle-button with text

--- src/components/ToggleButtonText/ToggleButtonText.tsx
+++ src/components/ToggleButtonText/ToggleButtonText.tsx
@@ -1,5 +1,35 @@
-function ToggleButtonText(): JSX.Element {
-  return <></>;
+import ToggleButton from '../ToggleButton';
+import styled from 'styled-components';
+
+const SizedToggleButton = styled(ToggleButton)`
+  width: 3rem;
+`
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+`;
+
+const Option = styled.h2`
+  font-size: 1.5rem;
+  font-style: normal;
+  font-weight: 400;
+`
+
+type ToggleButton = {
+  text: string,
+  activated: boolean,
+  onChange: () => void
+}
+
+function ToggleButtonText({ text, activated, onChange }: ToggleButton): JSX.Element {
+  return (
+    <Container>
+      <SizedToggleButton activated={activated} onChange={onChange}/>
+      <Option>{text}</Option>
+    </Container>
+  );
 }
 
 export default ToggleButtonText;
