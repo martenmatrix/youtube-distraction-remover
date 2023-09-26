6b6bbf0a56d2691876d5ac332e54aecd3bda5cb3
Marten <derapfelesser@aol.de>
2023-09-26 22:15:22 +0200

    fix(hideFeed): update selector

--- src/contents/selectors/homepage.ts
+++ src/contents/selectors/homepage.ts
@@ -1,3 +1,3 @@
 export default {
-  hideFeed: 'ytd-browse { visibility: hidden; width: 0px; height: 0px; }',
+  hideFeed: '.ytd-two-column-browse-results-renderer[is-default-grid] { visibility: hidden; width: 0px; height: 0px; }',
 };
