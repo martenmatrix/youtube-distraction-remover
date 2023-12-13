ed5e3121870963fcf876db9d560a98e90d75e060
Marten <derapfelesser@aol.de>
2023-12-13 23:20:17 +0100

    feat(popup): display option button in extension window

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,7 +1,7 @@
 import styled, { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './components';
 import { defaultTheme } from './colorThemes';
-import { ToggleButton } from './components';
+import { OptionButton } from './components';
 
 const Container = styled.div`
   // Chrome extension are allowed to be 800px wide and 600px high
@@ -14,7 +14,7 @@ function IndexPopup() {
     <Container>
       <ThemeProvider theme={defaultTheme}>
         <GlobalStyle />
-        <ToggleButton activated={false} onChange={() => {}} />
+        <OptionButton text="Hide Feed" active={true} />
       </ThemeProvider>
     </Container>
   );
