432796e15057e338107488a1efb98ac86617e896
Marten <derapfelesser@aol.de>
2023-12-18 23:18:54 +0100

    feat(react-testing-library): custom render method with theme provider

---
+++ test-utils.tsx
@@ -0,0 +1,18 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import { defaultTheme } from './src/colorThemes';
+
+const AllTheProviders = ({ children }) => {
+  return <ThemeProvider theme={defaultTheme}>{children}</ThemeProvider>;
+};
+
+const customRender = (ui, options) =>
+  render(ui, { wrapper: AllTheProviders, ...options });
+
+// re-export everything
+export * from '@testing-library/react';
+
+// override render method
+export { customRender as render };
