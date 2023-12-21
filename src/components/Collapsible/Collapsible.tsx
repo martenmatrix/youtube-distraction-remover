9678a167fd82f90d722dd25bac54fd0f3142004a
Marten <derapfelesser@aol.de>
2023-12-21 23:05:20 +0100

    feat(arrowicon): move logic to own component

--- src/components/Collapsible/Collapsible.tsx
+++ src/components/Collapsible/Collapsible.tsx
@@ -1,38 +1,13 @@
 import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 
+import StyledArrowIcon from './ArrowIcon';
+
 type Collapsible = {
   name: string;
   children?: React.ReactNode;
   className?: string;
 };
-
-const ArrowIconPath = styled.path.attrs({
-  d: 'M2 2L9 10L2 17',
-  strokeWidth: '3',
-  strokeLinecap: 'round',
-})`
-  stroke: ${(props) => props.theme.color.secondary};
-`;
-
-const ArrowIcon = ({ className }: { className?: string }) => {
-  return (
-    <svg
-      className={className}
-      width="12"
-      height="19"
-      viewBox="0 0 12 19"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg">
-      <ArrowIconPath />
-    </svg>
-  );
-};
-
-const StyledArrowIcon = styled(ArrowIcon)<{ $rotateBy?: string }>`
-  transition: transform 200ms;
-  transform: rotate(${(props) => props.$rotateBy});
-`;
 
 const CollapsibleButton = styled.div`
   display: flex;
