775ba4e731f3d01a9bd0d664309da577d6d51039
Marten <derapfelesser@aol.de>
2023-12-21 20:27:21 +0100

    style(collapsible): style collapsiblebutton

--- src/components/Collapsible/Collapsible.tsx
+++ src/components/Collapsible/Collapsible.tsx
@@ -7,7 +7,58 @@ type Collapsible = {
   className?: string;
 };
 
-const CollapsibleButton = styled.div``;
+const ArrowIconPath = styled.path.attrs({
+  d: 'M2 2L9 10L2 17',
+  strokeWidth: '3',
+  strokeLinecap: 'round',
+})`
+  stroke: ${(props) => props.theme.color.secondary};
+`;
+
+const ArrowIcon = ({ className }: { className?: string }) => {
+  return (
+    <svg
+      className={className}
+      width="12"
+      height="19"
+      viewBox="0 0 12 19"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg">
+      <ArrowIconPath />
+    </svg>
+  );
+};
+
+const StyledArrowIcon = styled(ArrowIcon)``;
+
+const CollapsibleButton = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  color: ${(props) => props.theme.color.secondary};
+  background: ${(props) => props.theme.color.primary};
+  cursor: pointer;
+
+  font-family: 'Work Sans', sans-serif;
+  font-size: 30px;
+  font-weight: 400;
+
+  width: 386px;
+  height: 67px;
+  padding: 0 10px;
+
+  border: 1px solid ${(props) => props.theme.color.secondary};
+  border-radius: ${(props) => props.theme.border.main};
+  box-shadow: 0 0 5px ${(props) => props.theme.color.primary};
+
+  /*
+  user-select is not an inherited property, though the initial auto value makes it behave like it is inherited most of 
+  the time. WebKit/Chromium-based browsers do implement the property as inherited, which violates the behavior described
+  in the spec, and this will bring some issues. See https://developer.mozilla.org/en-US/docs/Web/CSS/user-select.
+  */
+  user-select: none;
+`;
 
 const CollapsibleContent = styled.div``;
 
@@ -22,6 +73,7 @@ function Collapsible({
     <div className={className}>
       <CollapsibleButton aria-controls={name} aria-expanded={expanded}>
         {name}
+        <StyledArrowIcon />
       </CollapsibleButton>
       <CollapsibleContent id={name} aria-hidden={!expanded}>
         {children}
