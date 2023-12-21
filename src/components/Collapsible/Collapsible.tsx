6a8bdd475677f075effef952e9054eb0afa52b3a
Marten <derapfelesser@aol.de>
2023-12-21 16:59:32 +0100

    feat(collapsible): create html struct

---
+++ src/components/Collapsible/Collapsible.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+type Collapsible = {
+  name: string;
+  children: React.ReactNode;
+  className?: string;
+};
+
+const CollapsibleButton = styled.div``;
+
+const CollapsibleContent = styled.div``;
+
+function Collapsible({ name, children, className }): React.ReactNode {
+  const [expanded, setExpanded] = useState<boolean>(false);
+
+  return (
+    <div className={className}>
+      <CollapsibleButton aria-controls={name} aria-expanded={expanded}>
+        {name}
+      </CollapsibleButton>
+      <CollapsibleContent id={name} aria-hidden={!expanded}>
+        {children}
+      </CollapsibleContent>
+    </div>
+  );
+}
