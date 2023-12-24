a59e8b9e9f60d545dd6a3f84d47404ff2d4f172d
Marten <derapfelesser@aol.de>
2023-12-24 18:40:14 +0100

    feat(homepageListener): pass storage object as parameter

--- src/contents/listener/homepage.ts
+++ src/contents/listener/homepage.ts
@@ -3,9 +3,7 @@ import { Storage } from '@plasmohq/storage';
 import { homepage } from '../selectors';
 import editStylesheet from './editStylesheet';
 
-function setupHomepageListener(): void {
-  const storage = new Storage();
-
+function setupHomepageListener(storage: Storage): void {
   storage.watch({
     hideFeed: (active) => editStylesheet(active, homepage.hideFeed),
   });
