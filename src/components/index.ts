408ec397b8cc4592b45b2ef3b00e12cc69c4531f
Marten <derapfelesser@aol.de>
2023-12-25 20:21:35 +0100

    feat(components): export section component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import Collapsible from './Collapsible';
 import GlobalStyle from './GlobalStyle';
 import OptionButton from './OptionButton';
+import Section from './Section';
 
-export { GlobalStyle, OptionButton, Collapsible };
+export { GlobalStyle, OptionButton, Collapsible, Section };
