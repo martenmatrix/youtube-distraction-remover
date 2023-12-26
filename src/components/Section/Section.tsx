2b8eca4b6945f0a3592146e0e2c1d95744d1cb78
Marten <derapfelesser@aol.de>
2023-12-26 18:25:19 +0100

    types(styleSetting): declare as global type

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -4,11 +4,6 @@ import { useStorage } from '@plasmohq/storage/hook';
 
 import { Collapsible, OptionButton } from '../.';
 
-type StyleSetting = {
-  name: string; // A name for the setting, which will be displayed.
-  storageId: string; // The id, which is used to access the specific setting in the Storage API.
-};
-
 interface StyleSettingState extends StyleSetting {
   state: boolean;
   toggleState: () => any;
---
+++ typings/global.d.ts
@@ -0,0 +1,4 @@
+declare type StyleSetting = {
+  name: string; // A name for the setting, which will be displayed.
+  storageId: string; // The id, which is used to access the specific setting in the Storage API.
+};
