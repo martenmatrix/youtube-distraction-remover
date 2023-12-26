df94ee705ba99ba211cc5df58768582f30309acd
Marten <derapfelesser@aol.de>
2023-12-26 23:56:38 +0100

    type(styleSetting): only allow array

--- typings/global.d.ts
+++ typings/global.d.ts
@@ -2,5 +2,5 @@ declare type StyleSetting = {
   name: string; // A name for the setting, which will be displayed.
   storageId: string; // The id, which is used to access the specific setting in the Storage API.
   css: string; // The CSS, which should be applied to the document, when the setting is active.
-  section?: string[] | string; // The sections, where the setting should be listed.
+  section?: string[]; // The sections, where the setting should be listed.
 };
