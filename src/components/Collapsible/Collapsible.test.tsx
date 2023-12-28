9b6408f3296b6a1c2043428ea469e2fc48d0eae4
Marten <derapfelesser@aol.de>
2023-12-28 14:29:04 +0100

    test(collapsible): add Collapsible component test suite
    Implemented comprehensive unit tests for the Collapsible component. Tests verify proper rendering of passed props, correct display of aria-hidden attribute, and compatibility with screen reader navigation commands such as tab and enter.

---
+++ src/components/Collapsible/Collapsible.test.tsx
@@ -0,0 +1,62 @@
+import userEvent from '@testing-library/user-event';
+
+import { render, screen } from '../../../test-utils';
+import Collapsible from './Collapsible';
+
+describe('Collapsible', () => {
+  it('renders passed props and expands when clicked', async () => {
+    render(
+      <Collapsible name="A Collapsible">
+        <div>Children</div>
+      </Collapsible>,
+    );
+
+    const user = userEvent.setup();
+
+    const button = screen.getByRole('button', { name: /A Collapsible/i });
+    expect(button).toBeInTheDocument();
+    expect(screen.getByText('Children')).toBeInTheDocument();
+  });
+
+  it('displays aria-hidden for content correctly', async () => {
+    const result = render(
+      <Collapsible name="A Collapsible">
+        <div>Children</div>
+      </Collapsible>,
+    );
+    const user = userEvent.setup();
+
+    const collapsibleButton = screen.getByRole('button');
+    const collapsibleContent = result.container.querySelector(
+      'div[id="A Collapsible"]',
+    );
+
+    expect(collapsibleContent).toHaveAttribute('aria-hidden', 'true');
+    await user.click(collapsibleButton);
+    expect(collapsibleContent).toHaveAttribute('aria-hidden', 'false');
+  });
+
+  it('lets screen readers navigate with tab and enter', async () => {
+    const result = render(
+      <>
+        <Collapsible name="A Collapsible">
+          <div>Children</div>
+        </Collapsible>
+        <Collapsible name="A Second Collapsible">
+          <div>Children</div>
+        </Collapsible>
+      </>,
+    );
+    const user = userEvent.setup();
+
+    const collapsibleContent = result.container.querySelector(
+      'div[id="A Second Collapsible"]',
+    );
+
+    await user.tab();
+    await user.tab();
+    await user.keyboard('{Enter}');
+
+    expect(collapsibleContent).toHaveAttribute('aria-hidden', 'false');
+  });
+});
