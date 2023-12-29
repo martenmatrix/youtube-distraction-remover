6aa6cbd6047df7eb1ea96ecfdb0be523bfd48317
Marten <derapfelesser@aol.de>
2023-12-29 17:34:47 +0100

    fix(setupListeners): change argument type in setupListeners from Storage to BaseStorage
    The function setupListeners in setupListeners.ts now takes an argument of type BaseStorage from '@plasmohq/storage' instead of the general Storage type. The change simplifies import dependencies and ensures the function operates on the expected type.

--- src/listener/setupListeners.ts
+++ src/listener/setupListeners.ts
@@ -1,7 +1,9 @@
+import type { BaseStorage } from '@plasmohq/storage';
+
 import styleSettings from '../styleSettings';
 import editStylesheet from './editStylesheet';
 
-function setupListeners(storage: Storage): void {
+function setupListeners(storage: BaseStorage): void {
   styleSettings.forEach((setting) => {
     const callbackMap = {};
     callbackMap[setting.storageId] = (active: boolean) =>
