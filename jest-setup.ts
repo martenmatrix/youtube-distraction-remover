de2f8cc741f5ebe135d21577c7b46bed39c135e6
Marten <derapfelesser@aol.de>
2024-01-04 22:34:59 +0100

    test(jest-setup): mock document.adoptedStyleSheets

--- jest-setup.ts
+++ jest-setup.ts
@@ -1,2 +1,26 @@
 import '@testing-library/jest-dom';
 import '@jest/globals';
+
+interface ICSSStyleSheetMock {
+  cssRules: string[];
+  insertRule(rule: string): void;
+  deleteRule(index: number): void;
+}
+
+class CSSStyleSheetMock implements ICSSStyleSheetMock {
+  cssRules: string[] = [];
+
+  insertRule(rule: string) {
+    this.cssRules.push(rule);
+  }
+
+  deleteRule(index: number) {
+    this.cssRules.splice(index, 1);
+  }
+}
+
+// Mock document.adoptedStyleSheets with an array of CSSStyleSheetMock
+Object.defineProperty(document, 'adoptedStyleSheets', {
+  writable: true,
+  value: [new CSSStyleSheetMock()],
+});
