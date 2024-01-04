faea1c0962562644fde1f85a1de489fed40b1724
Marten <derapfelesser@aol.de>
2024-01-04 22:46:14 +0100

    test(restoreSettings): calls editStylesheet when a setting is active

---
+++ src/listener/restoreSettings.test.ts
@@ -0,0 +1,38 @@
+import { jest } from '@jest/globals';
+
+import type { BaseStorage } from '@plasmohq/storage';
+
+import editStylesheet from '~listener/editStylesheet';
+import restoreSettings from '~listener/restoreSettings';
+
+jest.mock('./editStylesheet', () => ({
+  __esModule: true,
+  default: jest.fn((active, css) => {}),
+}));
+
+describe('restoreSettings function', () => {
+  const mockStyleSettings: StyleSetting[] = [
+    {
+      name: 'Hide Feed',
+      storageId: 'hideFeed',
+      css: 'some css {}',
+      section: ['General', 'Homepage'],
+    },
+  ];
+
+  it('call editStylesheet when a setting is active', async () => {
+    const mockStorage = {
+      get: async (key) => {
+        return Promise.resolve(true);
+      },
+    } as unknown as BaseStorage;
+
+    await restoreSettings(mockStorage, mockStyleSettings);
+
+    expect(editStylesheet).toHaveBeenCalledWith(true, 'some css {}');
+  });
+
+  it('does not call editStylesheet when a setting is disabled', () => {});
+
+  it('does not call editStylesheet when a setting is null', () => {});
+});
