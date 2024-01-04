d29a9a3b27a06ee9b78dffe3c91a1bcde6f2cbb2
Marten <derapfelesser@aol.de>
2024-01-04 18:35:14 +0100

    feat(setupListeners): pass styleSettings as parameter instead importing

--- src/listener/setupListeners.ts
+++ src/listener/setupListeners.ts
@@ -1,9 +1,11 @@
 import type { BaseStorage } from '@plasmohq/storage';
 
-import styleSettings from '../styleSettings';
 import editStylesheet from './editStylesheet';
 
-function setupListeners(storage: BaseStorage): void {
+function setupListeners(
+  storage: BaseStorage,
+  styleSettings: StyleSetting[],
+): void {
   styleSettings.forEach((setting) => {
     const callbackMap = {};
     callbackMap[setting.storageId] = (c) =>
